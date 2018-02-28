310efbe7e7c04892b8e59800ec639885db110f43
piotr.welpa <piotr.welpa@intive.com>
2018-02-28 18:14:19 +0100

    feat: prepare MovieDbHelper and update MovieContract

--- app/src/main/java/com/example/piotrwelpa/popularmovies/data/database/MovieContract.java
+++ app/src/main/java/com/example/piotrwelpa/popularmovies/data/database/MovieContract.java
@@ -3,19 +3,19 @@ package com.example.piotrwelpa.popularmovies.data.database;
 import android.net.Uri;
 import android.provider.BaseColumns;
 
-public class MovieContract {
-    public static final String CONTENT_AUTHORITY = "com.example.piotrwelpa.popularmovies";
-    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+class MovieContract {
+    private static final String CONTENT_AUTHORITY = "com.example.piotrwelpa.popularmovies";
+    static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
 
-    public static final String PATH_MOVIE = "movie";
+    static final String PATH_MOVIE = "movie";
 
     public static final class MovieEntry implements BaseColumns {
         public static final Uri CONTENT_URI = BASE_CONTENT_URI.buildUpon()
                 .appendPath(PATH_MOVIE)
                 .build();
 
-        public static final String TABLE_NAME = "movie";
-        public static final String COLUMN_TITLE = "title";
+        static final String TABLE_NAME = "movie";
+        static final String COLUMN_TITLE = "title";
 
     }
 }
---
+++ app/src/main/java/com/example/piotrwelpa/popularmovies/data/database/MovieDbHelper.java
@@ -0,0 +1,37 @@
+package com.example.piotrwelpa.popularmovies.data.database;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import com.example.piotrwelpa.popularmovies.data.database.MovieContract.MovieEntry;
+
+
+public class MovieDbHelper extends SQLiteOpenHelper {
+
+    private static final String DATABASE_NAME = "movie.db";
+    private static final int DATABASE_VERSION = 0;
+
+    public MovieDbHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+        final String SQL_CREATE_WEATHER_TABLE =
+                "CREATE TABLE " + MovieEntry.TABLE_NAME + " (" +
+                        MovieEntry._ID + " REAL PRIMARY KEY, " +
+                        MovieEntry.COLUMN_TITLE + " TEXT NOT NULL, " +
+                        " UNIQUE (" + MovieEntry._ID + ") ON CONFLICT ROLLBACK);";
+
+        db.execSQL(SQL_CREATE_WEATHER_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + MovieEntry.TABLE_NAME);
+        onCreate(db);
+    }
+}
